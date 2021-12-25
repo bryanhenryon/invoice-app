f81c600db6959cdfc13653e26a80518170a5d728
Bryan <b.henryon@outlook.fr>
2021-12-25 19:33:21 +0100

    Add button to reset searchbar value

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,23 +1,48 @@
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 
 import { colors, breakpoints } from "../assets/style/variables";
 
 import { ReactComponent as SearchIcon } from "../assets/svg/icon-search.svg";
+import { ReactComponent as CloseIcon } from "../assets/svg/icon-close.svg";
 
-const Searchbar: React.FC = () => (
-  <InputContainer>
-    <SearchInput
-      type='text'
-      placeholder='Rechercher par client'
-      spellCheck='false'
-    />
-    <SearchIconExtended />
-  </InputContainer>
-);
+const Searchbar: React.FC = () => {
+  const [showDeleteIcon, setShowDeleteIcon] = useState(true);
+  const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    inputValue !== "" ? setShowDeleteIcon(true) : setShowDeleteIcon(false);
+  }, [inputValue]);
+
+  return (
+    <InputContainer>
+      <SearchInput
+        type='text'
+        placeholder='Rechercher par client'
+        spellCheck='false'
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      {showDeleteIcon ? (
+        <ResetInputButton onClick={() => setInputValue("")}>
+          <CloseIconExtended />
+        </ResetInputButton>
+      ) : (
+        <SearchIconExtended />
+      )}
+    </InputContainer>
+  );
+};
 
 const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
   width: 100%;
-  position: relative;
+  background: ${({ theme }) => theme.inputBackgroundColor};
+  border-radius: 0.4rem;
+  border: 1px solid ${({ theme }) => theme.inputBorderColor};
+  padding-left: 1.2rem;
+  min-height: 4rem;
 
   @media ${breakpoints.sm} {
     max-width: 27.5rem;
@@ -26,28 +51,40 @@ const InputContainer = styled.div`
 
 const SearchInput = styled.input`
   transition: 100ms ease-in;
-  background: ${({ theme }) => theme.inputBackgroundColor};
   color: ${({ theme }) => theme.blackToWhite};
-  padding: 1.2rem 3.5rem 1rem 1.2rem;
-  border-radius: 0.4rem;
+  background: ${({ theme }) => theme.inputBackgroundColor};
+  padding-right: 1.2rem;
   width: 100%;
-  border: 1px solid ${({ theme }) => theme.inputBorderColor};
   outline: none;
+  border: none;
 
   &::placeholder {
     color: ${colors.grey};
   }
 
   &:focus {
-    border-color: ${colors.violet};
+    ${InputContainer} {
+      border-color: ${colors.violet};
+    }
   }
 `;
 
+const ResetInputButton = styled.div`
+  display: flex;
+  margin-right: 1.2rem;
+  cursor: pointer;
+`;
+
+const CloseIconExtended = styled(CloseIcon)`
+  fill: ${colors.red};
+  height: 1.8rem;
+  width: 1.8rem;
+`;
+
 const SearchIconExtended = styled(SearchIcon)`
-  position: absolute;
-  right: 1.3rem;
-  top: 1rem;
   fill: ${colors.grey};
+  margin-right: 1.2rem;
+  height: 100%;
 `;
 
 export default Searchbar;
